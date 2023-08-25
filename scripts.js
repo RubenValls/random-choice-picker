0a733c8776486555792603e63eac9c033b1d5df2
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-25 09:06:26 +0200

    improved script to picker and lost the focus after pulse enter key

--- scripts.js
+++ scripts.js
@@ -2,13 +2,18 @@ const textarea = document.querySelector('#choices')
 const choices = document.querySelector('.choices-container')
 let choicesArray = []
 
-textarea.addEventListener('input', () => {
-    let choicesHtml = '';
-    choicesArray = textarea.value.split(',')
-    choicesArray.forEach((choice) => {
-        if(choice){
-            choicesHtml += `<div class="choice">${choice}</div>`
-        }
-    })
-    choices.innerHTML = choicesHtml;
+textarea.addEventListener("keydown", (event) => {
+    if(event.keyCode == 13){
+        textarea.value = '';
+        textarea.blur()
+    }else{
+        let choicesHtml = '';
+        choicesArray = textarea.value.split(',')
+        choicesArray.forEach((choice) => {
+            if(choice){
+                choicesHtml += `<div class="choice">${choice}</div>`
+            }
+        })
+        choices.innerHTML = choicesHtml;
+    }
 })
