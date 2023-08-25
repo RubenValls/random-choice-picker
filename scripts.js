79bfb34c70aa3c1fec63a2795c65a40d3230c9e8
Rubén Valls <r.vallsaparici@gmail.com>
2023-08-25 09:26:58 +0200

    added the selection but not included the selection animation effect yet

--- scripts.js
+++ scripts.js
@@ -2,10 +2,23 @@ const textarea = document.querySelector('#choices')
 const choices = document.querySelector('.choices-container')
 let choicesArray = []
 
+const aleatoryChoice = (allChoices) => {
+    allChoices.forEach((choice) => {
+        choice.style.backgroundColor =  '#EF932B';
+    })
+    const options = allChoices.length - 1;
+    const numberOption = Math.floor(Math.random() * (options - 0 + 1) + 0);
+    allChoices[numberOption].style.backgroundColor =  '#273C75';
+}
+
 textarea.addEventListener("keydown", (event) => {
     if(event.keyCode == 13){
+        const allChoices = document.querySelectorAll('.choice')
         textarea.value = '';
         textarea.blur()
+        if(allChoices.length > 0){
+            aleatoryChoice(allChoices)
+        }
     }else{
         let choicesHtml = '';
         choicesArray = textarea.value.split(',')
